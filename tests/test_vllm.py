1187723f53b6c11831efb3dd712a0d280d4994cc
Algopapi <darrynbiervliet@gmail.com>
2025-01-29 00:28:54 +0100

    vllm unit test (tbfixed)

--- tests/test_vllm.py
+++ tests/test_vllm.py
@@ -2,15 +2,23 @@ import pytest
 
 from relign.common.vllm_server import VLLMServer
 from relign.utils.gpu import get_gpu_memory
-
+from relign.episode_generators.on_policy_episode_generator import OnPolicyEpisodeGenerator
 
 @pytest.fixture
 def vllm_server():
     return VLLMServer()
 
+@pytest.fixture
+def epiosde_generator():
+    return OnPolicyEpisodeGenerator()
 
 class TestVLLMServer:
-    def test_start_and_stop_vllm(self, vllm_server: VLLMServer, tolarace=1.1):
+    def test_start_and_stop_vllm(
+        self, 
+        vllm_server: VLLMServer, 
+        episode_generator: OnPolicyEpisodeGenerator, 
+        tolarace=1.1
+    ):
         pre_gpu_mem_usage = get_gpu_memory()
 
         vllm_server.start_server(hf_ckpt_path_or_model="realtreetune/rho-1b-sft-GSM8K")
